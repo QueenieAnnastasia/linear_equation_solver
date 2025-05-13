0c2e6d5210a20406736ae91a0f420ba85ad04b16
QueenieAnnastasia <94047113+QueenieAnnastasia@users.noreply.github.com>
2025-05-13 22:29:51 +0700

    giai phuong trinh bac nhat

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Giải phương trình bậc nhất môt ẩn. Tìm x khi ax = b");
+        Console.WriteLine("Nhập a");
+        double a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Nhập b");
+        double b = Convert.ToDouble(Console.ReadLine());
+        if (a == 0)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Phương trình có vô số nghiệm");
+            }
+            else
+            {
+                Console.WriteLine("Phương trình vô nghiệm");
+            }
+        }
+        else
+        {   
+            double x = -b/a;
+            Console.WriteLine($"Nghiệm của phương trình là{x}");
+        }
+    }
+}
